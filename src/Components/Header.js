750cc88479ae0175cdb252107121b0f126e1aa20
cletus ogah <ogahcletus@gmail.com>
2022-08-24 02:41:56 +0100

    modify some spelling errors in header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -22,7 +22,7 @@ export default function Header() {
             <p> 
             I am an engineer, banking operations executive, project managegement professional, data analyst and a Software Developer.
             I have a burning passion for learning and research which has been useful in my nitch in solving and trouble shooting problems.
-            The object of this portfolio page is to create connections with prospective empoloyers, other software developers and clients so that we can engage in genuine and productive business discussions.
+            The object of this portfolio page is to create connections with prospective employers, other software developers and clients so that we can engage in genuine and productive business discussions.
             </p>
             
 
